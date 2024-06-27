2756a342576b1be8f96842c796c836591e334388
hoss7464 <150793911+hoss7464@users.noreply.github.com>
2024-06-27 14:03:42 +0330

    To add some changes in redux filter slice

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -14,7 +14,8 @@ import {
   setFilterMinPriceInput,
   setFilterMaxPriceInput,
   renderFilterMinMaxPriceInput,
-  resetMinMaxPriceInput ,
+  resetMinMaxPriceInput,
+  resetFilters,
 } from "../../Redux/actions/filterSlice";
 
 const Filter = () => {
@@ -24,73 +25,84 @@ const Filter = () => {
   const minPrice = useSelector((state) => state.filterProduct.minPrice);
   const maxPrice = useSelector((state) => state.filterProduct.maxPrice);
   const checkboxStates = useSelector((state) => state.filterProduct.checkboxStates);
-  const minPriceInput = useSelector((state) => state.filterProduct.minPriceInput)
-  const maxPriceInput = useSelector((state) => state.filterProduct.maxPriceInput)
-  
+  const minPriceInput = useSelector((state) => state.filterProduct.minPriceInput);
+  const maxPriceInput = useSelector((state) => state.filterProduct.maxPriceInput);
+  const filteredMinPrice = useSelector((state) => state.filterProduct.filteredMinPrice);
+  const filteredMaxPrice = useSelector((state) => state.filterProduct.filteredMaxPrice);
+
   const dispatch = useDispatch();
 
-  //Filter all products when the page loads and reloads : 
+  //Filter all products when the page loads and reloads :
   useEffect(() => {
     dispatch(renderAllProduct(db1));
   }, [dispatch]);
-  //Filter products based on input search : 
+  //---------------------------------------------------------------------
+  //Filter products based on input search :
   const handleSearchChange = (event) => {
     dispatch(renderSearchQuery(event.target.value));
   };
-
   const handleSearchClick = () => {
     dispatch(renderSearchInput(searchQuery));
     dispatch(renderFilteredProducts());
-    dispatch(resetPriceRange());
-    dispatch(resetCheckboxPriceRange());
-    dispatch(resetMinMaxPriceInput())
   };
-  //Filter products based on category button : 
+  //---------------------------------------------------------------------
+  //Filter products based on category button :
   const handleButtonClick = (category) => {
+    dispatch(resetFilters());
     dispatch(renderFilteredSelectedButton(category));
-    dispatch(resetPriceRange());
-    dispatch(resetCheckboxPriceRange());
-    dispatch(resetMinMaxPriceInput())
   };
+  //---------------------------------------------------------------------
   //Filter products based on input range :
   const handleRangeChange = (event) => {
     const newPriceRange = [minPrice, Number(event.target.value)];
     dispatch(renderFilteredPriceRange(newPriceRange));
     dispatch(resetCheckboxPriceRange());
-    dispatch(resetMinMaxPriceInput())
+    dispatch(resetMinMaxPriceInput());
   };
+  //----------------------------------------------------------------------
   //Filter products based on checkbox range :
   const handleCheckboxChange = (event, range) => {
     const { checked } = event.target;
     dispatch(renderFilteredCheckboxPriceRange({ checked, range }));
     dispatch(resetPriceRange());
-    dispatch(resetMinMaxPriceInput())
+    dispatch(resetMinMaxPriceInput());
   };
-  //Filter products based on min max input value : 
+  //----------------------------------------------------------------------
+  //Filter products based on min max input value :
   const handleMinPriceInputChange = (event) => {
     const value = Number(event.target.value);
     dispatch(setFilterMinPriceInput(value));
   };
-
   const handleMaxPriceInputChange = (event) => {
     const value = Number(event.target.value);
     dispatch(setFilterMaxPriceInput(value));
   };
-
   const handleMinMaxPriceInputClick = () => {
-    dispatch(renderFilterMinMaxPriceInput())
-  }
+    dispatch(renderFilterMinMaxPriceInput());
+    dispatch(resetCheckboxPriceRange());
+    dispatch(resetPriceRange());
+  };
 
   return (
     <>
-      <input
-        type="text"
-        placeholder="Search products..."
-        onChange={handleSearchChange}
-        value={searchQuery}
-      />
-      <button onClick={handleSearchClick}>Search</button>
       <div>
+        <h1>Min-Max price product :</h1>
+        <p>min product : {filteredMinPrice}</p>
+        <p>max product : {filteredMaxPrice}</p>
+      </div>
+      <div>
+        <h1>search input filter :</h1>
+        <input
+          type="text"
+          placeholder="Search products..."
+          onChange={handleSearchChange}
+          value={searchQuery}
+        />
+        <button onClick={handleSearchClick}>Search</button>
+      </div>
+
+      <div>
+        <h1>category button filter :</h1>
         <button onClick={() => handleButtonClick("all")}>All</button>
         <button onClick={() => handleButtonClick("kitchen")}>Kitchen</button>
         <button onClick={() => handleButtonClick("electronic")}>
@@ -101,80 +113,104 @@ const Filter = () => {
           Stationary
         </button>
       </div>
+
       <div>
-        <label for="vol">
-          Volume (between {minPrice} and {maxPrice}):
-        </label>
-        <input
-          type="range"
-          id="vol"
-          name="vol"
-          min={minPrice}
-          max={maxPrice}
-          value={priceRange[1]}
-          onChange={handleRangeChange}
-        />
-      </div>
-      <div>
-        <label>
+        <h1>Range Input Filter:</h1>
+        <label htmlFor="input-range">
+          Price Range: {priceRange[0]} - {priceRange[1]}
           <input
+            id="input-range"
+            type="range"
+            min={minPrice}
+            max={maxPrice}
+            value={priceRange[1]}
+            onChange={handleRangeChange}
+          />
+        </label>
+      </div>
+
+      <div>
+        <h1>checkbox filter :</h1>
+        <label htmlFor="checkbox-input1">
+          <input
+            id="checkbox-input1"
             type="checkbox"
             name="priceRange"
-            checked={checkboxStates.some(range => range[0] === 0 && range[1] === 1000)}
+            checked={checkboxStates.some(
+              (range) => range[0] === 0 && range[1] === 1000
+            )}
             onChange={(event) => handleCheckboxChange(event, [0, 1000])}
           />
           $0 - $1000
         </label>
-        <label>
+        <label htmlFor="checkbox-input2">
           <input
+            id="checkbox-input2"
             type="checkbox"
             name="priceRange"
-            checked={checkboxStates.some(range => range[0] === 1001 && range[1] === 2000)}
+            checked={checkboxStates.some(
+              (range) => range[0] === 1001 && range[1] === 2000
+            )}
             onChange={(event) => handleCheckboxChange(event, [1001, 2000])}
           />
           $1001 - $2000
         </label>
-        <label>
+        <label htmlFor="checkbox-input3">
           <input
+            id="checkbox-input3"
             type="checkbox"
             name="priceRange"
-            checked={checkboxStates.some(range => range[0] === 2001 && range[1] === 4000)}
+            checked={checkboxStates.some(
+              (range) => range[0] === 2001 && range[1] === 4000
+            )}
             onChange={(event) => handleCheckboxChange(event, [2001, 4000])}
           />
           $2001 - $4000
         </label>
-        <label>
+        <label htmlFor="checkbox-input4">
           <input
+            id="checkbox-input4"
             type="checkbox"
             name="priceRange"
-            checked={checkboxStates.some(range => range[0] === 4001 && range[1] === 6000)}
+            checked={checkboxStates.some(
+              (range) => range[0] === 4001 && range[1] === 6000
+            )}
             onChange={(event) => handleCheckboxChange(event, [4001, 6000])}
           />
           $4001 - $6000
         </label>
-        <label>
+        <label htmlFor="checkbox-input5">
           <input
+            id="checkbox-input5"
             type="checkbox"
             name="priceRange"
-            checked={checkboxStates.some(range => range[0] === 6001 && range[1] === 18000)}
+            checked={checkboxStates.some(
+              (range) => range[0] === 6001 && range[1] === 18000
+            )}
             onChange={(event) => handleCheckboxChange(event, [6001, 18000])}
           />
           $6001 - $18000
         </label>
-        <label>
+        <label htmlFor="checkbox-input6">
           <input
+            id="checkbox-input6"
             type="checkbox"
             name="priceRange"
-            checked={checkboxStates.some(range => range[0] === 18001 && range[1] === 38000)}
+            checked={checkboxStates.some(
+              (range) => range[0] === 18001 && range[1] === 38000
+            )}
             onChange={(event) => handleCheckboxChange(event, [18001, 38000])}
           />
           $18001 - $38000
         </label>
       </div>
+
       <div>
-      <label>
+        <h1>min max price input filter :</h1>
+        <label htmlFor="min-price-input">
           Min Price:
           <input
+            id="min-price-input"
             type="number"
             value={minPriceInput}
             onChange={handleMinPriceInputChange}
@@ -182,9 +218,10 @@ const Filter = () => {
             max={maxPrice}
           />
         </label>
-        <label>
+        <label htmlFor="max-price-input">
           Max Price:
           <input
+            id="max-price-input"
             type="number"
             value={maxPriceInput}
             onChange={handleMaxPriceInputChange}
@@ -192,8 +229,11 @@ const Filter = () => {
             max={maxPrice}
           />
         </label>
-        <button onClick={handleMinMaxPriceInputClick}>Apply Price Filter</button>
+        <button onClick={handleMinMaxPriceInputClick}>
+          Apply Price Filter
+        </button>
       </div>
+
       {filteredProducts.map((product) => (
         <div key={product.id}>
           <h2>{product.header}</h2>
--- src/Redux/actions/filterSlice.js
+++ src/Redux/actions/filterSlice.js
@@ -1,85 +1,129 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  products: [],
+  filteredProducts: [],
+  minPrice: 0,
+  maxPrice: 0,
+  priceRange: [0, 0],
+  searchInput: "",
+  searchQuery: "",
+  selectedButton: "all",
+  checkboxStates: [],
+  minPriceInput: 0,
+  maxPriceInput: 38000,
+  filteredMinPrice: 0,
+  filteredMaxPrice: 0,
+};
+
 export const filterSlice = createSlice({
   name: "filterProduct",
-  initialState: {
-    products: [],
-    searchInput: "",
-    searchQuery: "",
-    filteredProducts: [],
-    selectedButton: "all",
-    priceRange: [0, 38000],
-    minPrice: 0,
-    maxPrice: 38000,
-    selectedPriceRange: [],
-    checkboxStates: [],
-    minPriceInput: 0,
-    maxPriceInput: 38000,
-  },
+  initialState,
   reducers: {
-    //when product page loads , it renders all products on the page.
+    //To render whole products when page loads :
     renderAllProduct: (state, action) => {
       state.products = action.payload;
       state.filteredProducts = action.payload;
-      //To change the price range min max price based on filtered product's min max price (if you have price range filter in your project)
+      //(To show filtered products based on price range)
       state.minPrice = Math.min(...action.payload.map((p) => p.price));
-      state.maxPricePrice = Math.max(...action.payload.map((p) => p.price));
+      state.maxPrice = Math.max(...action.payload.map((p) => p.price));
       state.priceRange = [state.minPrice, state.maxPrice];
+      //(To show the value of price input based on min max price of whole products)
+      state.minPriceInput = state.minPrice;
+      state.maxPriceInput = state.maxPrice;
+      //(To show min max price of filtered products on top of the page)
+      state.filteredMinPrice = state.minPrice;
+      state.filteredMaxPrice = state.maxPrice;
     },
-    //Filter products based on input search.
+    //To render products based on search input :
     renderSearchInput: (state, action) => {
       state.searchInput = action.payload;
+      //(To erase category button filter)
+      state.selectedButton = "all";
     },
     renderSearchQuery: (state, action) => {
       state.searchQuery = action.payload;
     },
+    //To render products based on all types of filter :
     renderFilteredProducts: (state) => {
+      let products = state.products;
+      //------------------------------------------------------------------------
+      // Apply search filter :
       const searchTextLower = state.searchInput.toLowerCase();
-      state.selectedButton = "all";
-      state.filteredProducts = state.products.filter((product) => {
-        return (
-          product.header.toLowerCase().includes(searchTextLower) ||
-          product.paragraph.toLowerCase().includes(searchTextLower) ||
-          product.brand.toLowerCase().includes(searchTextLower) ||
-          product.category.toLowerCase().includes(searchTextLower) ||
-          product.product.toLowerCase().includes(searchTextLower)
-        );
-      });
-      //To change the price range min max price based on filtered product's min max price (if you have price range filter in your project)
-      state.minPrice = Math.min(...state.filteredProducts.map((p) => p.price));
-      state.maxPrice = Math.max(...state.filteredProducts.map((p) => p.price));
-      state.priceRange = [state.minPrice, state.maxPrice];
-    },
-    //Filter products based on button click (category, brand and ...)
-    renderFilteredSelectedButton: (state, action) => {
-      state.selectedButton = action.payload;
-      if (state.selectedButton === "all") {
-        state.filteredProducts = state.products;
-      } else {
-        state.filteredProducts = state.products.filter((product) => {
+      if (searchTextLower) {
+        //To filter searched products based on header, brand, category, product
+        products = products.filter((product) => {
+          return (
+            product.header.toLowerCase().includes(searchTextLower) ||
+            product.brand.toLowerCase().includes(searchTextLower) ||
+            product.category.toLowerCase().includes(searchTextLower) ||
+            product.product.toLowerCase().includes(searchTextLower)
+          );
+        });
+      }
+      //------------------------------------------------------------------------
+      // Apply category filter
+      if (state.selectedButton !== "all") {
+        products = products.filter((product) => {
           return product.category === state.selectedButton;
         });
       }
-      //To change the price range min max price based on filtered product's min max price (if you have price range filter in your project)
-      state.minPrice = Math.min(...state.filteredProducts.map((p) => p.price));
-      state.maxPrice = Math.max(...state.filteredProducts.map((p) => p.price));
-      state.priceRange = [state.minPrice, state.maxPrice];
-    },
-    //Filter products based on price range :
-    renderFilteredPriceRange: (state, action) => {
-      state.priceRange = action.payload;
-      state.filteredProducts = state.products.filter((product) => {
+      //------------------------------------------------------------------------
+      // Apply min-max input price range filter
+      products = products.filter((product) => {
         return (
-          product.price >= state.priceRange[0] &&
-          product.price <= state.priceRange[1]
+          product.price >= state.minPriceInput &&
+          product.price <= state.maxPriceInput
         );
       });
+      //------------------------------------------------------------------------
+      // Apply checkbox price range filter
+      if (state.checkboxStates.length > 0) {
+        products = products.filter((product) => {
+          return state.checkboxStates.some(
+            (range) => product.price >= range[0] && product.price <= range[1]
+          );
+        });
+      }
+
+      if (
+        state.priceRange[0] !== state.minPrice ||
+        state.priceRange[1] !== state.maxPrice
+      ) {
+        products = products.filter((product) => {
+          return (
+            product.price >= state.priceRange[0] &&
+            product.price <= state.priceRange[1]
+          );
+        });
+      }
+      //------------------------------------------------------------------------
+      state.filteredProducts = products;
+      state.filteredMinPrice = Math.min(...products.map((p) => p.price));
+      state.filteredMaxPrice = Math.max(...products.map((p) => p.price));
+    },
+    //To render products based on category buttons :
+    renderFilteredSelectedButton: (state, action) => {
+      state.selectedButton = action.payload;
+      filterSlice.caseReducers.resetFilters(state);
+      filterSlice.caseReducers.renderFilteredProducts(state);
+    },
+    //To render products based on input range
+    renderFilteredPriceRange: (state, action) => {
+      state.searchInput = "";
+      state.searchQuery = "";
+      state.selectedButton = "all";
+      state.checkboxStates = [];
+      state.minPriceInput = 0;
+      state.maxPriceInput = 38000;
+      state.priceRange = action.payload;
+      filterSlice.caseReducers.renderFilteredProducts(state);
     },
     resetPriceRange: (state) => {
-      //To change the price range min max price based on filtered product's min max price (if you have price range filter in your project)
       state.priceRange = [state.minPrice, state.maxPrice];
+      filterSlice.caseReducers.renderFilteredProducts(state);
     },
-    //Filter products based on checkbox :
+    //To render products based on checkboxes :
     renderFilteredCheckboxPriceRange: (state, action) => {
       const { checked, range } = action.payload;
       if (checked) {
@@ -90,22 +134,13 @@ export const filterSlice = createSlice({
             selectedRange[0] !== range[0] || selectedRange[1] !== range[1]
         );
       }
-      if (state.checkboxStates.length === 0) {
-        state.selectedPriceRange = [];
-        state.filteredProducts = state.products;
-      } else {
-        state.selectedPriceRange = state.checkboxStates;
-        state.filteredProducts = state.products.filter((product) =>
-          state.selectedPriceRange.some(
-            ([min, max]) => product.price >= min && product.price <= max
-          )
-        );
-      }
+      filterSlice.caseReducers.renderFilteredProducts(state);
     },
     resetCheckboxPriceRange: (state) => {
       state.checkboxStates = [];
+      filterSlice.caseReducers.renderFilteredProducts(state);
     },
-    //Filter based on maximum and minimum input price range :
+    //To render products based on min max price input :
     setFilterMinPriceInput: (state, action) => {
       state.minPriceInput = action.payload;
     },
@@ -113,17 +148,23 @@ export const filterSlice = createSlice({
       state.maxPriceInput = action.payload;
     },
     renderFilterMinMaxPriceInput: (state) => {
-      state.filteredProducts = state.products.filter((product) => {
-        return (
-          product.price >= state.minPriceInput &&
-          product.price <= state.maxPriceInput
-        );
-      });
+      filterSlice.caseReducers.renderFilteredProducts(state);
     },
-    resetMinMaxPriceInput : (state) => {
-        state.minPriceInput = state.minPrice
-        state.maxPriceInput = state.maxPrice
-    }
+    resetMinMaxPriceInput: (state) => {
+      state.minPriceInput = 0;
+      state.maxPriceInput = 38000;
+      filterSlice.caseReducers.renderFilteredProducts(state);
+    },
+    //To reset all filtered actions and back to default :
+    resetFilters: (state) => {
+      state.searchInput = "";
+      state.searchQuery = "";
+      state.priceRange = [state.minPrice, state.maxPrice];
+      state.checkboxStates = [];
+      state.minPriceInput = 0;
+      state.maxPriceInput = 38000;
+      filterSlice.caseReducers.renderFilteredProducts(state);
+    },
   },
 });
 
@@ -140,7 +181,8 @@ export const {
   setFilterMinPriceInput,
   setFilterMaxPriceInput,
   renderFilterMinMaxPriceInput,
-  resetMinMaxPriceInput ,
+  resetMinMaxPriceInput,
+  resetFilters,
 } = filterSlice.actions;
 
 export default filterSlice.reducer;
